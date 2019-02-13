9c59f3676e3df26c4b98edd5a966151dc413b07b
erachinpawar <46299719+erachinpawar@users.noreply.github.com>
2019-02-13 12:15:46 +0530

    Changed the configuration to seconds

--- Assignment/src/main/java/com/cswm/assignment/ApplicationConstants.java
+++ Assignment/src/main/java/com/cswm/assignment/ApplicationConstants.java
@@ -10,7 +10,7 @@ public class ApplicationConstants {
 	public static final String DEFAULT_USER = "Default User";
 	public static final String CLOSED_STATUS = "close";
 	public static final String APPLICATION_SHUTDOWN_MSG = "Application closed now ....";
-	public static final Long GRACEFUL_SHUTDOWN_WINDOW = 30000l; /// IN MILI-SECONDS
+	public static final Long GRACEFUL_SHUTDOWN_WINDOW = 30l; /// IN SECONDS
 	public static final String APPLICATION_NAME = "Instrument order management system";
 	public static final String APPLICATION_NAME_DESC = "This page lists all the Orders placed.";
 	public static final String APPLICATION_NAME_VERSION = "1.0-SNAPSHOT";
